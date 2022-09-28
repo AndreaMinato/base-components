8fe2480a446316fdab051181ca01e6e0f05621d8
Andrea Minato <andreaminato@outlook.com>
2022-09-28 17:27:39 +0200

    Add Text fields

---
+++ components/NInput.vue
@@ -0,0 +1,23 @@
+<template>
+  <input class="form-input form-input__tempestive" v-model="value" />
+</template>
+
+<script lang="ts" setup>
+import { computed } from "vue";
+
+const props = defineProps({
+  modelValue: {},
+});
+const emit = defineEmits(["update:modelValue"]);
+
+const value = computed({
+  get() {
+    return props.modelValue;
+  },
+  set(value: unknown) {
+    emit("update:modelValue", value);
+  },
+});
+</script>
+
+<style></style>
